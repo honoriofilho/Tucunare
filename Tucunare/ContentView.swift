68e387de38a2717180d11a34e0a46525b2165044
honoriofilho <honoriovitor.web@gmail.com>
2025-06-10 09:54:29 -0300

    first commit

---
+++ Tucunare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tucunare
+//
+//  Created by honorio on 10/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tucunare/TucunareApp.swift
@@ -0,0 +1,17 @@
+//
+//  TucunareApp.swift
+//  Tucunare
+//
+//  Created by honorio on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TucunareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
